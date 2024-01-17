86f777cb5b65847de71eaff150c96b54efdd2855
ohmigoo <48247501+ohmigoo@users.noreply.github.com>
2024-01-17 02:34:55 -0800

    Add files via upload

---
+++ app/components/copyrights.tsx
@@ -0,0 +1,13 @@
+// copyrights.tsx
+
+import React from 'react';
+
+const Copyrights: React.FC = () => {
+  return (
+    <footer>
+      <p>&copy; 2017 Your Company Name. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Copyrights;
